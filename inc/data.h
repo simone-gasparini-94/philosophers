4cc9a4beea53ffaa68d85f806bcc2e144f912ef6
Simone Gasparini <sgaspari@c3a2c7.42berlin.de>
2025-09-22 16:23:38 +0200

    Create new structure for philosopher
    The new structure would be specific for every philosopher
    which would have an unique id.
    The structure has only a pointer to the global structure, so that
    the other variables can still be access from within

--- inc/data.h
+++ inc/data.h
@@ -6,7 +6,7 @@
 /*   By: sgaspari <sgaspari@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 13:33:44 by sgaspari          #+#    #+#             */
-/*   Updated: 2025/09/16 13:33:52 by sgaspari         ###   ########.fr       */
+/*   Updated: 2025/09/22 16:20:48 by sgaspari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,25 @@
 # define DATA_H
 
 # include <sys/types.h>
+# include <pthread.h>
+
+typedef struct s_data t_data;
+
+typedef struct s_philo
+{
+	t_data		*data;
+	size_t		id;
+}	t_philo;
 
 typedef struct s_data
 {
-	size_t	num_philo;
-	size_t	time_to_die_ms;
-	size_t	time_to_eat_ms;
-	size_t	time_to_sleep_ms;
-	size_t	num_times_philo_must_eat;
+	t_philo		*philo;
+	pthread_t	*threads;
+	size_t		num_philo;
+	size_t		time_to_die_ms;
+	size_t		time_to_eat_ms;
+	size_t		time_to_sleep_ms;
+	size_t		num_times_philo_must_eat;
 }	t_data;
 
 #endif
--- src/init.c
+++ src/init.c
@@ -6,13 +6,17 @@
 /*   By: sgaspari <sgaspari@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 12:44:43 by sgaspari          #+#    #+#             */
-/*   Updated: 2025/09/16 12:45:35 by sgaspari         ###   ########.fr       */
+/*   Updated: 2025/09/22 16:22:58 by sgaspari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "data.h"
 #include "utils.h"
+
+static void init_threads(t_data *data);
+static void	init_philo(t_data *data);
 
 void	init(t_data *data, char **argv)
 {
@@ -21,4 +25,37 @@ void	init(t_data *data, char **argv)
 	data->time_to_eat_ms = str_to_num(argv[3]);
 	data->time_to_sleep_ms = str_to_num(argv[4]);
 	data->num_times_philo_must_eat = str_to_num(argv[5]);
+	data->threads = malloc(sizeof(pthread_t) * data->num_philo);
+	init_threads(data);
+	init_philo(data);
+}
+
+static void init_threads(t_data *data)
+{
+	data->threads = malloc(sizeof(pthread_t) * data->num_philo);
+	if (data->threads == NULL)
+	{
+		perror("malloc");
+		exit (EXIT_FAILURE);
+	}
+}
+
+static void	init_philo(t_data *data)
+{
+	size_t	i;
+
+	data->philo = malloc(sizeof(t_philo) * data->num_philo);
+	if (data->philo == NULL)
+	{
+		free(data->threads);
+		perror("malloc");
+		exit (EXIT_FAILURE);
+	}
+	i = 0;
+	while (i < data->num_philo)
+	{
+		data->philo[i].id = i;
+		data->philo[i].data = data;
+		i++;
+	}
 }
